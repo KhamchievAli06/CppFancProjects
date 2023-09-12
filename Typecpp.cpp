4a95b8f297167868d6bfebda3b17a9e052bf331f
KhamchievAli06 <144795257+KhamchievAli06@users.noreply.github.com>
2023-09-12 13:59:23 +0300

    create TypesCpp

---
+++ Typecpp.cpp
@@ -0,0 +1,51 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = -2147483648;
+    int b = 2147483637;
+    long long c = -4294967276;
+    long long d = 4294967277;
+    short a2 = SHRT_MAX;
+    short a3 = SHRT_MIN;
+    unsigned a4 = 0;
+    unsigned int a5 = UINT32_MAX;
+    unsigned long long a6 = ULLONG_MAX;
+    unsigned short a7 = USHRT_MAX;
+    float b1 = FLT_MAX;
+    float b2 = FLT_MIN;
+    double b3 = DBL_MAX;
+    double b4 = DBL_MIN;
+    char b5 = CHAR_MAX;
+    char b6 = CHAR_MIN;
+    bool b7 = 0;
+    bool b8 = 0;
+   
+    cout << "typ0\t\t|byte\t|min\t\t\t|max\n";
+    cout << "int\t\t|" << sizeof(a) << "\t|" << a << "\t\t|" << b << "\n";
+    cout << "long\t\t|" << sizeof(c) << "\t|" << c << "\t\t|" << d << "\n";
+    cout << "short\t\t|" << sizeof(a2) << "\t|" << a2 << "\t\t\t|" << a3 << "\n";
+    cout << "unsigned int\t\t|" << sizeof(a5) << "\t|" << a5 << "\t\t\t|" << a6 << "\n";
+
+        /*
+    int a = -2147483648;
+    int b = 2147483637;
+    long long c = -4294967276;
+    long long d = 4294967277;
+    short a2 = SHRT_MAX;
+    short a3 = SHRT_MIN;
+    unsigned a4 = 0;
+    unsigned int a5 = UINT32_MAX;
+    unsigned long long a6 = ULLONG_MAX;
+    unsigned short a7 = USHRT_MAX;
+    float b1 = FLT_MAX;
+    float b2 = FLT_MIN;
+    double b3 = DBL_MAX;
+    double b4 = DBL_MIN;
+    char b5 = CHAR_MAX;
+    char b6 = CHAR_MIN;
+    bool b7 = 0;
+    bool b8 = 0;
+ */
+}
