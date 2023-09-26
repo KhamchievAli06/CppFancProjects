c852f50c9bbaefc173838f312300349b07c8581d
KhamchievAli06 <144795257+KhamchievAli06@users.noreply.github.com>
2023-09-26 12:54:39 +0300

    Add files via upload
    creat TypesCpp

---
+++ calc swith case.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    //calculator (only output result + - * /
+    float a, b;
+    char op;
+    cout << "Input a ";
+    cin >> a;
+    cout << "Input b ";
+    cin >> b;
+    cout << "Выберите действие: +,-,:,*: ";
+    cin >> op;
+    switch (op) {
+    case '+':
+        cout << a + b;
+        break;
+    case '-':
+        cout << a - b;
+        break;
+    case '*':
+        cout << a * b;
+        break;
+    case '/':
+        cout << a / b;
+        break;
+    default:
+        cout << "Ошибка";
+        break;
+    }
+}
