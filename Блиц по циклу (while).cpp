604d15da132f25f9d3b5e37e15f63eb889b2b8f5
KhamchievAli06 <144795257+KhamchievAli06@users.noreply.github.com>
2023-10-24 12:20:35 +0300

    Add files via upload
    Блиц по циклу (while)

---
+++ Блиц по циклу (while).cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+int main(){
+    setlocale(LC_ALL, "");
+    /*int a = 1;
+    while (a <= 10) {
+        cout << a << " ";
+        a ++;
+    }*/
+    /*int a, b;
+    cout << "input: ";
+    cin >> a >> b;
+    if (a > b) swap(a, b);
+    while (b >= a) {
+        cout << b << " ";
+        --b;
+    }*/
+    int a, b = 0;
+    cout << "input: ";
+    cin >> a;
+    while (a != 0) {
+        b += a;
+        cout << "input: ";
+        cin >> a;
+    }
+    cout << b;
+}
